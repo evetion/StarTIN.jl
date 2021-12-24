33c8ac381b248e39b01d8604252ddc364231104c
Maarten Pronk <git@evetion.nl>
2021-12-24 11:39:33 +0100

    PR for unreleased version.

--- src/StarTIN.jl
+++ src/StarTIN.jl
@@ -1,6 +1,8 @@
 module StarTIN
 using Libdl
 using startin_jll
+# libstartin = joinpath(pwd(), "../startin/target/release/libstartin.dylib")
+
 
 struct Star
     pt::Vector{Float64} # length 3
@@ -29,9 +31,23 @@ end
 
 
 function Base.insert!(t::DT, points::Matrix{Float64})
-    w, h = size(points)
+    w, _ = size(points)
     w == 3 || error("Point array should be 3 dimensional, got $w dimensions.")
     res = ccall((:insert, libstartin), Cint, (Ptr{Triangulation}, Cint, Ref{Cdouble}), t.ptr, length(points), points)
+    res == 0 || @warn "$res duplicate points encountered."
+    res
+end
+
+function Base.insert!(t::DT, point::Vector{Float64})
+    w = length(point)
+    w == 3 || error("Point array should be 3 dimensional, got $w dimensions.")
+    res = ccall((:insert_one_pt, libstartin), Cint, (Ptr{Triangulation}, Cdouble, Cdouble, Cdouble), t.ptr, point[1], point[2], point[3])
+    res == 0 || @warn "Error inserting point."
+    res
+end
+
+function Base.delete!(t::DT, pointid)
+    res = ccall((:remove, libstartin), Cint, (Ptr{Triangulation}, Cint), t.ptr, pointid)
     res == 0 || @warn "$res duplicate points encountered."
     res
 end
@@ -40,11 +56,21 @@ function info(t::DT)
     ccall((:debug, libstartin), Cint, (Ptr{Triangulation},), t.ptr)
 end
 
+function get_snap_tolerance(t::DT)
+    ccall((:get_snap_tolerance, libstartin), Cdouble, (Ptr{Triangulation},), t.ptr)
+end
+function set_snap_tolerance!(t::DT, tol::Float64)
+    ccall((:set_snap_tolerance, libstartin), Cdouble, (Ptr{Triangulation}, Cdouble), t.ptr, tol)
+end
+
 function interpolate_nn(t::DT, x::Float64, y::Float64)
     ccall((:interpolate_nn, libstartin), Cdouble, (Ptr{Triangulation}, Cdouble, Cdouble), t.ptr, x, y)
 end
 function interpolate_linear(t::DT, x::Float64, y::Float64)
     ccall((:interpolate_linear, libstartin), Cdouble, (Ptr{Triangulation}, Cdouble, Cdouble), t.ptr, x, y)
+end
+function interpolate_nni(t::DT, x::Float64, y::Float64)
+    ccall((:interpolate_nni, libstartin), Cdouble, (Ptr{Triangulation}, Cdouble, Cdouble), t.ptr, x, y)
 end
 function interpolate_laplace(t::DT, x::Float64, y::Float64)
     ccall((:interpolate_laplace, libstartin), Cdouble, (Ptr{Triangulation}, Cdouble, Cdouble), t.ptr, x, y)
@@ -60,10 +86,12 @@ function destroy!(t::DT)
 end
 
 export DT
-export insert!
 export interpolate_nn
+export interpolate_nni
 export interpolate_linear
 export interpolate_laplace
+export get_snap_tolerance
+export set_snap_tolerance!
 export write!
 export destroy!
 export info
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,31 @@ using Test
     using StarTIN
 
     t = DT()
+
+    # Insert multiple points
     points = rand(0.0:0.01:100.0, 3, Int(1e5))
     @test @time insert!(t, points) > 0
 
+    # Insert single point
+    @test @time insert!(t, [50.0, 50.0, 50.0]) > 0
+
+    # Insert invalid points
     points = rand(4, 100)
     @test_throws Exception insert!(t, points)
+    @test_throws Exception insert!(t, [1.0, 2.0])
 
+    # Test removal
+    pid = insert!(t, [51.0, 51.0, 50.0])
+    @test delete!(t, pid) == 0
+
+    # Test tolerance
+    @test get_snap_tolerance(t) == 0.001
+    @test set_snap_tolerance!(t, 0.00001) == 0.00001
+
+    # Test interpolation
     @test !isnan(interpolate_linear(t, 0.5, 0.5))
     @test !isnan(interpolate_nn(t, 0.5, 0.5))
+    @test !isnan(interpolate_nni(t, 0.5, 0.5))
     @test !isnan(interpolate_laplace(t, 0.5, 0.5))
 
     fn = "test.obj"
